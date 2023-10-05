66974e50817fe9238e112e8223eb9945a3d2e303
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-10-05 18:05:03 -0300

    feat: wip

--- posts_app/models.py
+++ posts_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Posts(models.mMdel):
+class Posts(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='images/')
