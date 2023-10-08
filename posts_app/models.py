da2bf8ee9fa72791113bb30c6b5369b93dbf7d22
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-10-08 08:48:06 -0300

    feat: wip

--- posts_app/models.py
+++ posts_app/models.py
@@ -6,3 +6,6 @@ class Posts(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='images/')
     create_at =models.DateTimeField(auto_now_add=True)#adiciona automaticamente
+
+    def __str__(self): #recebendo o titulo para que fique visivel no blog
+        return self.title
